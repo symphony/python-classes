06ffa610de146e9c9687362e06d02aad8a1473b4
symphony <jciurc@gmail.com>
2022-07-30 08:21:17 -0700

    create camera and position classes

--- securityDevices.py
+++ securityDevices.py
@@ -1,11 +1,57 @@
+from enum import Enum
+
+
 class SecurityDevice:
-    def __init__(self, active, reset):
+    def __init__(self, active=False):
         self.active = active
-        self.reset = reset
 
-    def isActive():
-        print('hi')
+    def reset(self):
+        print('Resetting device..')
+        self.active = True
+
+    def getDeviceStatus(self):
+        print('Active: ', self.active)
 
 
-device1 = SecurityDevice
-device1.isActive()
+class Sensor(SecurityDevice):
+    def __init__(self, silent, distance):
+        self.silent = silent
+        self.distance = distance
+
+
+class Camera(SecurityDevice):
+    def __init__(self, serial, position, type):
+        self.serial = serial
+        self.position = position
+        self.type = type
+
+    def aboutCamera(self):
+        print(
+            'Serial: ', self.serial,
+            'Positon:', self.position.getPosition(),
+            '\nCamera Type: ', self.type,
+        )
+
+    class CameraType(Enum):
+        a: 1
+        b: 2
+        c: 3
+
+
+class Position:
+    def __init__(self, pan, tilt, zoom):
+        self.pan = pan
+        self.tilt = tilt
+        self.zoom = zoom
+
+    def getPosition(self):
+        return (self.pan, self.tilt, self.zoom)
+
+        # test runner
+device1 = SecurityDevice(False)
+sensor1 = Sensor(False, 30)
+cam1 = Camera('abc-123', Position(10, 11, 12), Camera.CameraType.a)
+# sensor1.getDeviceStatus()
+sensor1.reset()
+sensor1.getDeviceStatus()
+cam1.aboutCamera()
