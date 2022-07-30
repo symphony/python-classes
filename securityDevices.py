76470e567b7b2d031e65d14454e009bdf509a32a
symphony <jciurc@gmail.com>
2022-07-30 07:49:08 -0700

    create base security class

---
+++ securityDevices.py
@@ -0,0 +1,11 @@
+class SecurityDevice:
+    def __init__(self, active, reset):
+        self.active = active
+        self.reset = reset
+
+    def isActive():
+        print('hi')
+
+
+device1 = SecurityDevice
+device1.isActive()
