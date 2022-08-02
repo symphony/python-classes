a210e809d936ffecaac3f543fb8e2872ecf624b6
symphony <jciurc@gmail.com>
2022-08-01 18:24:19 -0700

    create more methods. fix getter/setter in Node class

--- linkedList.py
+++ linkedList.py
@@ -1,40 +1,48 @@
-from turtle import end_fill
-
-
 class Node:
     def __init__(self, data) -> None:
         self.data = data
-        self._next
-
-    def next(self, data):
-        # setter
-        if data:
-            self._next = Node(data)
-        # getter
-        else:
-            return self._next
+        self.next = None
 
 
 class LinkedList:
-    def __init__(self, start) -> None:
+    def __init__(self, start: Node) -> None:
         self.start = start
 
-    def add(self, index, data):
-        if (index == 0):
+    def add(self, index: int, data):
+        # add to beginning
+        if index == 0:
             node = Node(data)
-            node.next(self.start)
+            node.next = self.start
             self.start = node
+        # add at index
         else:
             node = LinkedList.get(index - 1)
-            node.next(data)
+            node.next = data
 
-    def delete(self):
-        pass
+    def append(self, data):
+        node = self.start
+        while node.next != None:
+            node = node.next
+        node.next = Node(data)
 
-    def get(self, index):
+    def delete(self, index: int):
+        # delete beginning
+        if index == 0:
+            self.start == self.start.next
+        # delete at index
+        else:
+            node = self.start
+            for i in range(index - 1):
+                node = node.next
+
+            toDelete = node.next
+            node.next = toDelete.next
+            toDelete.next = None
+
+    def get(self, index: int):
         node = self.start
         for i in range(index):
-            node = node.next()
+            node = node.next
         return node
 
 
