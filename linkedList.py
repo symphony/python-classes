c4d69652e65b9d1e6eae4874358e5c46764a62ca
symphony <jciurc@gmail.com>
2022-08-01 18:01:20 -0700

    create linkedlist add, next, get methods

--- linkedList.py
+++ linkedList.py
@@ -4,22 +4,38 @@ from turtle import end_fill
 class Node:
     def __init__(self, data) -> None:
         self.data = data
+        self._next
 
-    def next():
-        pass
+    def next(self, data):
+        # setter
+        if data:
+            self._next = Node(data)
+        # getter
+        else:
+            return self._next
 
 
 class LinkedList:
     def __init__(self, start) -> None:
         self.start = start
 
-    def add():
+    def add(self, index, data):
+        if (index == 0):
+            node = Node(data)
+            node.next(self.start)
+            self.start = node
+        else:
+            node = LinkedList.get(index - 1)
+            node.next(data)
+
+    def delete(self):
         pass
 
-    def delete():
-        pass
+    def get(self, index):
+        node = self.start
+        for i in range(index):
+            node = node.next()
+        return node
 
-    def get():
-        pass
 
-
+n1 = Node(5)
